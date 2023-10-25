0133a59fc49cc362b6facd80f836e70b98aefee0
john j <johnjjdev690@gmail.com>
2023-10-25 19:20:56 +0000

    git add

--- contracts/Common.sol
+++ contracts/Common.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ReentrancyGuard.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 //import "hardhat/console.sol";
